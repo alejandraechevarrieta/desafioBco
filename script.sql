2509f206f5f1ad2b00b62fd7ec21bba85afc9f60
Alejandra Echevarrieta <alechevarrieta@gmail.com>
2023-05-15 15:21:57 -0300

    script

--- script.sql
+++ script.sql
@@ -1,4 +1,3 @@
-
 --1) correr 
 
 
@@ -11,11 +10,7 @@ CREATE DATABASE banco;
 
 USE [banco]
 GO
-/****** Object:  Table [dbo].[Clientes]    Script Date: 15/5/2023 10:13:20 ******/
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Clientes]') AND type in (N'U'))
-DROP TABLE [dbo].[Clientes]
-GO
-/****** Object:  Table [dbo].[Clientes]    Script Date: 15/5/2023 10:13:20 ******/
+/****** Object:  Table [dbo].[Clientes]    Script Date: 15/5/2023 15:15:12 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -32,6 +27,13 @@ PRIMARY KEY CLUSTERED
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+SET IDENTITY_INSERT [dbo].[Clientes] ON 
+GO
+INSERT [dbo].[Clientes] ([idCliente], [tarjeta], [password], [dni], [nombre]) VALUES (1, 12345678, N'1234', 22222222, N'Juan')
+GO
+SET IDENTITY_INSERT [dbo].[Clientes] OFF
+GO
+
 
 USE [banco]
 GO
@@ -52,5 +54,8 @@ PRIMARY KEY CLUSTERED
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+
+
+
 
 
